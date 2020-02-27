0fad34219dd089f32c3e0cfdd18d576f8032b29d
snowadmirer <pengjiade@gmail.com>
2020-02-27 21:01:13 +0800

    Update keras_vgg16.py

--- nets/keras_vgg16.py
+++ nets/keras_vgg16.py
@@ -21,14 +21,8 @@ def VGG16(input_shape,
     inputs = Input(shape=input_shape)
     
     # Block 1
-    x = Conv2D(64, (3, 3),
-                      activation='relu',
-                      padding='same',
-                      name='block1_conv1')(inputs)
-    x = Conv2D(64, (3, 3),
-                      activation='relu',
-                      padding='same',
-                      name='block1_conv2')(x)
+    x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv1')(inputs)
+    x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv2')(x)
     x = MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool')(x)
 
     # Block 2
